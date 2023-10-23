000ec65f19419a11e03c090b0023dcb28c9af0a8
ndrada <andrada.groseanu@gmail.com>
2023-10-23 07:22:04 -0700

    finished program
    added wpm test phrases to the text.txt file that will be randomly picked by the program for the user to test their typing speed. Added another function for that (load_text), and updated some of the previous code

--- wpm.py
+++ wpm.py
@@ -1,5 +1,7 @@
 import curses
 from curses import wrapper
+import time
+import random
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -10,27 +12,45 @@ def start_screen(stdscr):
 
 def display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
-        stdscr.addstr(0, i, char, curses.color_pair(1))
+        color = curses.color_pair(1)
 
+        if char != correct_char:
+            color = curses.color_pair(2)
+        
+        stdscr.addstr(0, i, char, color)
 
-
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+    
 def wpm_test(stdscr):
-    target_text = 'Hello world! this is some test text for this app'
+    target_text = load_text()
     current_text = []
+    wpm = 0
+    start_time = time.time() #keeps track of the start time
+    stdscr.nodelay(True)
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
         stdscr.clear() 
-        # stdscr.addstr(target_text)
-        display_text(stdscr, target_text, current_text)
-        # for char in current_text:
-        #     stdscr.addstr(char, curses.color_pair(1))
-        
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
 
         if ord(key) == 27:
             break
@@ -38,7 +58,7 @@ def wpm_test(stdscr):
         if key in ('KEY_BACKSPACE', '\b', '\x7f'):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        elif len(current_text) < len(target_text):
             current_text.append(key)
 
 def main(stdscr):
@@ -47,7 +67,13 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press any key to continue.")
+        key = stdscr.getkey()
 
-wrapper(main) #call function and initialize wrapper
+        if ord(key) == 27:
+            break
+
+wrapper(main)
 
